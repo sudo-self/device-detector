4e96abdf36134011889cbe3c3f510a97f3faa036
sudo-self <sudo-self@proton.me>
2025-07-03 13:24:59 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", 
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? "/device-detector" : "",
+  assetPrefix: isGithubPages ? "/device-detector/" : "",
 };
 
 export default nextConfig;
+
